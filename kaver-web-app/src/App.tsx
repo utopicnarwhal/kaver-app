2d07ff6d33530803400995f23feec9f82a781be3
Сергей Данилов <gigok@bk.ru>
2019-10-07 00:13:30 +0300

    init web-app

---
+++ kaver-web-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './kaver-logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        HELLO WORLD
+      </header>
+    </div>
+  );
+}
+
+export default App;
