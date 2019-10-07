9d5e0993fe15abe4ed95df65b9692d8d4596eb0d
Sergey <gigok@bk.ru>
2019-10-07 14:39:08 +0300

    update readme.md and init main page

--- kaver-web-app/src/app/App.tsx
+++ kaver-web-app/src/app/App.tsx
@@ -6,9 +6,20 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        HELLO WORLD
+        <div className="Logo-block">
+          <img src={logo} className="App-logo" alt="Логотип" />
+          <span className="App-logo-text">KAVER</span>
+        </div>
+        <div className="Login-block">
+          <span className="App-login-text">Log in</span>
+        </div>
       </header>
+      <div id="main">
+        <div className="Card"></div>
+      </div>
+      <div className="App-footer">
+        <div className="Copyright">Copyright</div>
+      </div>
     </div>
   );
 }
